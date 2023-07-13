a135486f39e932df56560c6cb8963cbe19f5854f
zoe <z.g.bradford@gmail.com>
2023-07-13 11:24:49 -0500

    homepage changed depending on if there is a user

---
+++ movie-project-app/src/components/Homepage/LoggedOut.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import CardComponent from './CardComponent';
+
+
+
+function LoggedOut(props) {
+ 
+  return (
+    <div>
+        <div>
+            COMING SOON
+        </div>
+        <div className="container col-md-12 border">
+          <div className='row'>
+            {props.movieItems.map((item) => (
+              <CardComponent
+                id={item.id}
+                image={props.IMG_URL + item.poster_path}
+                title={item.title}
+                score={item.vote_average}
+                overview={item.overview}
+                
+              />
+            ))}
+          </div>  
+        </div>
+    </div>
+  );
+}
+
+
+export default LoggedOut;
