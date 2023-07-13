15c9756a2471579e4e026fbdb8e00eb15787534c
zoe <z.g.bradford@gmail.com>
2023-07-13 09:50:53 -0500

    homepage layout and api calling

--- movie-project-app/src/components/Homepage/CardComponent.js
+++ movie-project-app/src/components/Homepage/CardComponent.js
@@ -1,16 +1,19 @@
 import React from 'react';
 
 
+
+
 function CardComponent(props) {
  
   return (
-    <div className="movie" style={{ cursor: 'pointer'}}>
-      <h3>{props.title}</h3>
+    <div className="text-center p-2 border" style={{ cursor: 'pointer'}}>
+      {/* <h3>{props.title}</h3> */}
       <img className="border mt-2 border-dark rounded" src={props.image} alt={props.title} />
-      <div>Score: {props.score}</div>
-      <div>Overview: {props.overview}</div>
+      {/* <div>Score: {props.score}</div> */}
+      {/* <div>Overview: {props.overview}</div> */}
     </div>
   );
 }
+
 
 export default CardComponent;
