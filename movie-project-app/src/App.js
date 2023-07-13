b85446d4dcc25d1f6961305381a4c5decdf50def
zoe <z.g.bradford@gmail.com>
2023-07-12 20:48:29 -0500

    setting up routes

--- movie-project-app/src/App.js
+++ movie-project-app/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route, Link } from 'react-router-dom';
+import Homepage from './components/Homepage/Homepage';
+//import MoviePage from './components/MoviePage/MoviePage';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+   
+
+
+
+
+
+    return (
+      <div>
+        <h2>Hulix</h2>
+        
+        <Routes>
+          <Route path="/" element={<Homepage 
+          
+          />} />
+          
+           {/* <Route path="/MoviePage" element={<MoviePage
+      
+          />} />   */}
+          
+        </Routes>
+      </div>
+    );
+  }
+
 
 export default App;
---
+++ movie-project-app/src/components/Homepage/Homepage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+function Homepage() {
+   
+
+  return (
+    <div>
+      This is the home page
+      
+    </div>
+  );
+}
+
+export default Homepage;
